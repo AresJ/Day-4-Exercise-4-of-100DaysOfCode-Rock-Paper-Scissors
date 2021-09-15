6d538c0eda5ed3dd4c03cd894e72a250f38fa0fb
Carlil Jean <jaedagk@live.com>
2021-09-15 02:53:41 +0000

    Edited code to include game images and rearranged If/Else flow.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -24,15 +25,25 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+gameImages = [rock, paper, scissors]
+playersChoice = int(input("Let's play Rock-Paper-Scissors! \n Select 0 for Rock, 1 for Paper, or 2 for Scissors:"))
 
-playersChoice = input("Let's play Rock-Paper-Scissors! \n Select 0 for Rock, 1 for Paper, or 2 for Scissors:")
-import random
-computerChoice = random.randint(0, 2)
-if computerChoice == 2:
-  print(rock)
-elif computerChoice == 1:
-  print(paper)
-elif computerChoice == 0:
-  print(scissors)
+if playersChoice >= 3 or playersChoice < 0: 
+  print("You typed an invalid number. You lose!")
 else: 
-  print("Game Over!")
+  print(gameImages[playersChoice])
+
+  computerChoice = random.randint(0, 2)
+  print(f"Computer chose {computerChoice}")
+  print(gameImages[computerChoice])
+
+  if playersChoice == 0 and computerChoice == 2:
+    print("You Win!")
+  elif playersChoice == 2 and computerChoice == 0:
+    print("You lose")  
+  elif  computerChoice > playersChoice:
+    print("You lose!")
+  elif  playersChoice > computerChoice:
+    print("You win!")
+  elif computerChoice == playersChoice:
+    print("Its a draw!")
